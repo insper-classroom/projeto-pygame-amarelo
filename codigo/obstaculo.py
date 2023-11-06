4d2b9b9999f440ac0abc417e34d62bb954c450b6
joaorcmr <joaoricardoribeiro01@gmail.com>
2023-11-06 15:10:05 -0300

    commit antes de fazer caca

--- codigo/obstaculo.py
+++ codigo/obstaculo.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Obstacle:
     def __init__(self, window):
@@ -6,9 +7,13 @@ class Obstacle:
         Classe que representa os obstaculos.
         pygame.Surface: A superfície da janela do jogo.
         """
+        self.score = 0
         self.window = window
         self.obstacles = []
         self.passed = False
+        self.obstacles_passed = []
+    
+    
     def initialize_obstacle(self, obstacle_type, x, y, width=None, height=None):
         """
         Inicializa um obstáculo.
@@ -41,6 +46,35 @@ class Obstacle:
         }
         obstacle_images['mask'] = pygame.mask.from_surface(obstacle_images['image'])
         self.obstacles.append(obstacle_images)
+
+    def generate_obstacle_pair(self, obstacle_type, gap_size, x_position):
+        """
+        Gera um par de obstáculos com uma lacuna entre eles.
+        obstacle_type: O tipo de obstáculo ('clif' ou 'nuddle').
+        gap_size: O tamanho do espaço entre os obstáculos superior e inferior.
+        x_position: A posição x inicial para os obstáculos.
+        """
+        width = 100  # Largura padrão para os obstáculos 'clif' e 'nuddle'
+        total_height = self.window.get_height()
+
+        # A altura do obstáculo de baixo é aleatória, mas deixa espaço para a lacuna e o obstáculo de cima
+        bottom_height = random.randint(200, 700)  # 200 é um valor mínimo de exemplo para a altura do obstáculo superior
+
+        # O obstáculo de baixo começa na parte inferior da tela menos sua altura
+        bottom_y_position = total_height - bottom_height
+
+        # O obstáculo de cima é o que resta do espaço menos a lacuna
+        top_height = total_height - bottom_height - gap_size
+
+        # O obstáculo de cima começa no topo da tela (y=0)
+        top_y_position = 0
+
+        # Inicializa o obstáculo inferior
+        self.initialize_obstacle(obstacle_type, x_position, bottom_y_position, width, bottom_height)
+
+        # Inicializa o obstáculo superior
+        self.initialize_obstacle(obstacle_type, x_position, top_y_position, width, top_height)
+
     def update(self):
         """
         Atualiza a posição dos obstáculos na tela.
@@ -58,6 +92,9 @@ class Obstacle:
                 if obstacle['type'] == 'nuddle':
                     obstacle['x'] = 3600
                     obstacle['hitbox'].x = 3600
+
+
+
     def draw(self):
         """
         Desenha os obstáculos na tela.
